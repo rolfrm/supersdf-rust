62e8fd5e50658bf0874d6746079548b172addb4b
romadsen <rolfrm@gmail.com>
2023-07-18 22:44:54 +0200

    optimized edits a lot

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ use kiss3d::scene::SceneNode;
 use image::{Rgba};
 
 use kiss3d::nalgebra::{
-    Point2, Point3, Vector2, Vector3,
+    Point2, Point3, Vector2, Vector3, Translation, Translation3,
 };
 use kiss3d::window::{State, Window};
 
@@ -95,7 +95,7 @@ impl SdfScene {
         let cell_distance = (cell_position - self.eye_pos).norm();
         
         // Calculate the LOD level based on the distance of the cell.
-        let lod_level = (cell_distance * 0.5 / self.block_size).log2().floor().max(0.0);
+        let lod_level = (cell_distance * 0.5 / self.block_size).log2().floor().max(0.0) * 0.5;
         
         // Calculate the cell size. Farther away -> bigger blocks with lower resolution.
         let lod_cell_size = self.block_size * 2.0_f32.powf(lod_level);
@@ -106,7 +106,7 @@ impl SdfScene {
                 x: cell_position.x as i32,
                 y: cell_position.y as i32,
                 z: cell_position.z as i32,
-                w: lod_cell_size as i32,
+                w: cell_size as i32,
             };
 
             self.callback(key, cell_position, cell_size, &omodel, cell_size, lod_level);
@@ -132,7 +132,7 @@ impl SdfScene {
 
 struct AppState {
     sdf_iterator: SdfScene,
-    nodes: HashMap<SdfKey, (SceneNode, DistanceFieldEnum)>,
+    nodes: HashMap<SdfKey, (SceneNode, DistanceFieldEnum, f32, Vec3)>,
     texture_manager: TextureManager,
     cursor_pos: Vec2,
     camera: ArcBall,
@@ -141,8 +141,8 @@ struct AppState {
 impl AppState {
     pub fn new(sdf_iterator: SdfScene) -> AppState {
         //let cam = FirstPerson::new(Point3::new(0.0,0.0,-5.0), Point3::new(0.0, 0.0, 0.0));
-        let cam = ArcBall::new(Point3::new(0.0, 0.0, -5.0), Point3::new(0.0, 0.0, 0.0));
-
+        let mut cam = ArcBall::new_with_frustrum(1.0, 0.1, 1000.0, Point3::new(0.0, 0.0, -5.0), Point3::new(0.0, 0.0, 0.0));
+        
         AppState {
             sdf_iterator,
             nodes: HashMap::new(),
@@ -184,14 +184,14 @@ impl State for AppState {
 
                     if let Some((_, p)) = col {
                         let newobj = Sphere::new(p, 0.5).color(Rgba([255, 0, 0, 255]));
-                        self.sdf_iterator.sdf = self.sdf_iterator.sdf.Insert2(newobj);
+                        self.sdf_iterator.sdf = self.sdf_iterator.sdf.Insert2(newobj).optimize_bounds();
                         
-                        for node in self.nodes.iter_mut() {
-                            let n = &mut node.1.0;    
-                            n.unlink();
-                        }
+                        //for node in self.nodes.iter_mut() {
+                        //    let n = &mut node.1.0;    
+                        //    n.unlink();
+                        //}
                         
-                        self.nodes.clear();
+                        //self.nodes.clear();
                         
                         
                     }
@@ -208,30 +208,43 @@ impl State for AppState {
         self.sdf_iterator.eye_pos = self.camera.eye().to_homogeneous().xyz();
         
         //self.c.prepend_to_local_rotation(&self.rot);
-        self.sdf_iterator.iterate_scene(centerpos, 256.0);
+        self.sdf_iterator.iterate_scene(centerpos, 64.0);
 
         for node in self.nodes.iter_mut() {
             let n = &mut node.1.0;    
             n.set_visible(false);
         }
         
-
         for block in &self.sdf_iterator.render_blocks {
+            loop {
             let nd = self.nodes.entry(block.2).or_insert_with(|| {
-                println!("Node at: {:?}", block);
+
+
 
                 let pos = block.2;
+                
                 let size = pos.w as f32;
-
+                let pos = block.0;
+                
                 let mut r = VertexesList::new();
-                println!("Optimize: {:?}", block.3);
+                //println!("Optimize: {:?}", block.3);
                 let newsdf = block.3.optimized_for_block(block.0, size);
-                println!("Optimized: {:?}", newsdf);
-                println!("Scale: {}", block.4);
+                //println!("Optimized: {:?}", newsdf);
+                
                 marching_cubes_sdf(&mut r, &newsdf, block.0, size, 0.2 * 2.0_f32.powf(block.4));
                 
-                println!("Mc done: {:?}", r.len());
                 if r.any() {
+                    println!("Cube: {} ({} {} {})", size, pos.x, pos.y, pos.z);
+                
+                    //let mut cube = win.add_cube(1.0, 1.0, 1.0);
+                    //
+                    //cube.set_local_scale(size * 1.99, size* 1.99, size* 1.99);
+                    //
+                    //cube.set_local_translation(Translation3::new(pos.x as f32, pos.y as f32, pos.z as f32));
+                    //
+                    //return (cube, block.3.clone(), size, pos);
+                    
+    
                     let meshtex = r.to_mesh(&newsdf);
 
                     let name = format!("{:?}", block.2).to_string();
@@ -244,12 +257,25 @@ impl State for AppState {
                         win.add_mesh(Rc::new(meshtex.0.into()), Vec3f::new(1.0, 1.0, 1.0));
                     node.set_texture(tex2);
                     println!("Builrt node!");
-                    return (node, block.3.clone());
+                    return (node, block.3.clone(), size, pos);
                 } else {
-                    return (win.add_group(), block.3.clone());
+                    return (win.add_group(), block.3.clone(), size, pos);
                 }
             });
-            nd.0.set_visible(true);
+            //println!("{} ({} {} {}) {}", nd.2, nd.3.x,nd.3.y,nd.3.z, nd.1.eq(&block.3));
+        
+            
+            if(!nd.1.eq(&block.3)){ 
+                // node changed since last time.
+                nd.0.unlink();
+                self.nodes.remove(&block.2);
+            }else{
+
+                nd.0.set_visible(true);
+                break;
+            }
+        }
+            
             
         }
     }
@@ -257,6 +283,9 @@ impl State for AppState {
 
 
 fn main() {
+    let sdf : DistanceFieldEnum = Sphere::new(Vec3::new(0.0, 0.0, 0.0), 2.0)
+        .color(Rgba([255, 0, 0, 255]))
+        .into();// build_test();
     let sdf = build_test();
     let sdf2 = sdf.optimize_bounds();
 
@@ -268,6 +297,7 @@ fn main() {
     };
 
     let mut window = Window::new("Kiss3d: wasm example");
+    
 
     window.set_light(Light::StickToCamera);
 
@@ -289,7 +319,7 @@ mod tests {
         let mut sdf_iterator = SdfScene {
             sdf: sdf2,
             eye_pos: Vec3::zeros(),
-            block_size: 4.0,
+            block_size: 2.0,
             render_blocks: Vec::new(),
         };
 
